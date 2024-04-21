bc07c7b574974a0b674b7f45d3062b9892ce2416
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-04-21 19:51:49 -0400

    add documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
-"""starts connection to  API"""
-import os
+"""Defines the main Flask app for the API"""
+from flask import Flask
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
-from flask_cors import CORS
+import os
 
-
+# Creating the Flask app
 app = Flask(__name__)
+
+# Registering the blueprint
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+# CORS
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
+# Status route
 @app.teardown_appcontext
-def teardown_appcontext(code):
-    """teardown_appcontext"""
+def close_storage(self):
     storage.close()
 
 
+# Error handling
 @app.errorhandler(404)
-def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def handle_not_found_error(error):
+    """Handles 404 errors by returning a JSON-formatted response"""
+    return {"error": "Not found"}, 404
+
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+
+    host = os.getenv("HBNB_API_HOST")
+    port = os.getenv("HBNB_API_PORT")
+    if not host:
+        host = "0.0.0.0"
+    if not port:
+        port = "5000"
+
+    app.run(host=host, port=port, threaded=True)
